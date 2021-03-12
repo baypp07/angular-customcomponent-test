1d09752a15fd59505fa6eeb51d0cd35bb5e188e1
baypp07 <pphouone@gmail.com>
2021-03-12 18:01:42 +0100

    1 file modified

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -2,6 +2,18 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "custom",
-  template: "<h2>test</h2>"
+  template: `
+    <h2>{{ "this is : " + getTitle() }}</h2>
+    <ul *ngFor="let exemple of exemples">
+      <li>{{ exemple }}</li>
+    </ul>
+  `
 })
-export class TestComponent {}
+export class TestComponent {
+  title = "liste of my exemple ngFor";
+  exemples = ["ex1", "ex2", "ex3"];
+
+  getTitle() {
+    return this.title;
+  }
+}
